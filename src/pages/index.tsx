1633905af9006590594435a6f8e4e8b5a813a285
josiahschuller <josiahschuller@gmail.com>
2024-03-01 11:04:56 +1100

    Added play button link from homepage to play page and passed players to play page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,46 @@
 import React from "react";
 import RegisterPlayers from "../components/registerPlayers";
 import Title from "../components/title";
+import { useRouter } from 'next/router';
 
 export default function Home() {
   const [players, setPlayers] = React.useState<string[]>([]);
 
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-      <Title/>
+      <Title />
       <RegisterPlayers players={players} setPlayers={setPlayers}></RegisterPlayers>
+      <PlayGameButton players={players} />
+    </div>
+  );
+}
+
+function PlayGameButton(props: { players: string[] }) {
+  const { players } = props;
+  const router = useRouter();
+
+  const cardWidth: string = "400px";
+
+  return (
+    <div
+      style={{
+        margin: "1rem",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        width: cardWidth,
+      }}
+    >
+      <button
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        style={{ width: "180px", height: "45px", marginTop: 5 }}
+        onClick={() => {router.push({
+          pathname: "/play",
+          query: {players: encodeURIComponent(JSON.stringify(players))}
+        })}}
+      >
+        Play game
+      </button>
     </div>
   );
 }
--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 import Game from '../components/game';
 import Title from '../components/title';
+import { useRouter } from 'next/router';
 
 export default function Play() {
+    // Get players from query parameters
+    const router = useRouter();
+    const encodedPlayers: string | string[] = router.query.players || [];
+    let players: string[] = [];
+    if (Array.isArray(encodedPlayers)) {
+        // If encodedPlayers is an array, map over it and decode each element
+        players = encodedPlayers.map((player) => decodeURIComponent(player));
+    } else {
+        // If encodedPlayers is a string, decode it and parse it back into an array
+        players = [decodeURIComponent(encodedPlayers)];
+    }
+
     return (
         <div>
             <Title></Title>
