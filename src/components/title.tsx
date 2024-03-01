62ac07e8270c9b269e211d0bf6c2dc13dd588b16
josiahschuller <josiahschuller@gmail.com>
2024-03-01 12:39:11 +1100

    Added point tracking to game

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 export default function GameTitle() {
-    return (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-            <h1>{"Children's Quiz"}</h1>
-        </div>
-    );
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+      <h1 className="mb-2 mt-0 text-5xl font-medium leading-tight text-primary">
+        {"Children's Quiz"}
+      </h1>
+    </div>
+  );
 }
--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Game from '../components/game';
 import Title from '../components/title';
 import { useRouter } from 'next/router';
+import { Player } from '../types/player';
 
 export default function Play() {
-    // Get players from query parameters
-    const router = useRouter();
+  // Get players from query parameters
+  const router = useRouter();
+  const [playersState, setPlayersState] = React.useState<Player[]>([]);
+
+  useEffect(() => {
     const encodedPlayers: string | string[] = router.query.players || [];
     let players: string[] = [];
     if (Array.isArray(encodedPlayers)) {
-        // If encodedPlayers is an array, map over it and decode each element
-        players = encodedPlayers.map((player) => decodeURIComponent(player));
+      // If encodedPlayers is an array, map over it and decode each element
+      players = encodedPlayers.map((player) => decodeURIComponent(player));
     } else {
-        // If encodedPlayers is a string, decode it and parse it back into an array
-        players = [decodeURIComponent(encodedPlayers)];
+      // If encodedPlayers is a string, decode it and parse it back into an array
+      players = JSON.parse(
+        decodeURIComponent(encodedPlayers) // Decode the string
+          .replace(/'/g, '"') // Replace single quotes with double quotes
+      );
     }
 
-    return (
-        <div>
-            <Title></Title>
-            <Game></Game>
-        </div>
-    );
+    // Set players in state
+    setPlayersState(players.map((player) => {
+      return {
+        name: player,
+        points: 0,
+      };
+    }));
+  }, [router.query.players]);
+
+  return (
+    <div>
+      <Title />
+      <Game
+        playersState={playersState}
+        setPlayersState={setPlayersState}
+      />
+    </div>
+  );
 };
